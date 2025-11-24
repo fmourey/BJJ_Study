29fe09c308ee3bdb6183183b70662d4403fd1df1
Lina Benzakour <160588237+linabenzakour@users.noreply.github.com>
2025-11-24 12:49:56 +0100

    Page ajouter vidéos

--- BJJ_Study_frontend/src/router/index.js
+++ BJJ_Study_frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import VideoDetailView from '../views/VideoDetailView.vue'
 import SearchVideoView from '../views/SearchVideoView.vue'
+import AddVideoView from '../views/AddVideoView.vue' 
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/search',
     name: 'search',
     component: SearchVideoView
+  },
+  {
+    path: '/addvideo',
+    name: 'add-video',
+    component: AddVideoView
   }
 ]
 
