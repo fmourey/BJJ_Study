1c1eefe90fc1692ffec865ae6757afe01a13d516
naisatlan <atlan.nais.06@gmail.com>
2026-02-03 14:34:46 +0100

    Corrections pb git

--- BJJ_Study_frontend/src/main.js
+++ BJJ_Study_frontend/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createAuth0 } from "@auth0/auth0-vue"
 
 import App from './App.vue'
 import router from './router'
@@ -9,4 +10,17 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
-app.mount('#app')
+
+createApp(App)
+  .use(router)
+  .use(
+    createAuth0({
+      domain: import.meta.env.VITE_AUTH0_DOMAIN,
+      clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+      },
+    })
+  )
+  .mount("#app");
