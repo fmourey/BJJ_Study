7698df7f6c509a448a6b1dd3e3710e94e7e4888f
naisatlan <atlan.nais.06@gmail.com>
2026-03-02 14:34:16 +0100

    test CI

--- BJJ_Study_frontend/src/main.js
+++ BJJ_Study_frontend/src/main.js
@@ -19,7 +19,7 @@ createApp(App)
       domain: import.meta.env.VITE_AUTH0_DOMAIN,
       clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
       authorizationParams: {
-        redirect_uri: window.location.origin,
+        redirect_uri: window.location.origin + "/static/",
         audience: import.meta.env.VITE_AUTH0_AUDIENCE,
       },
     cacheLocation: "localstorage",
